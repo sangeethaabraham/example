88db36a6cb687b35001c09b284aa56839824433c
sangeetha <sangeetha_abraham@yahoo.com>
2018-08-28 12:25:28 +0530

    Original Version
    Version1.0

---
+++ Item-Catalog/src/main/java/com/example/demo/ItemCatalogApplication.java
@@ -0,0 +1,68 @@
+package com.example.demo;
+
+import java.util.stream.Stream;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Component;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ItemCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemCatalogApplication.class, args);
+	}
+}
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+class Item{
+	public Item(){};
+	public Item(String name){
+		this.name = name;
+	}
+	
+	@Id
+	@GeneratedValue
+	private long id;
+	
+	private String name;
+}
+
+@RepositoryRestResource
+interface ItemRepository extends JpaRepository<Item, Long>{}
+
+@Component
+class ItemInitializer implements CommandLineRunner{
+	
+	private final ItemRepository itemRepository;
+
+	ItemInitializer(ItemRepository itemRepository){
+		this.itemRepository = itemRepository;
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		Stream.of("Lining", "PUMA", "Bad Bot", "Nike", "Reebok", 
+				"Adidas").forEach(item -> itemRepository.save(new Item(item)) );
+		itemRepository.findAll().forEach(System.out::println);
+	}
+	
+}
---
+++ currency-conversion-service/src/main/java/com/example/demo/CurrencyConversionServiceApplication.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClients;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients("com.example.demo")
+@RibbonClients({
+@RibbonClient("name=forex-service")})
+public class CurrencyConversionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
+	}
+}
